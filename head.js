29888dd81ea7c88e6ca98e141a88010d0261e982
Noor Chasib <noorchasib@gmail.com>
2022-11-21 09:32:01 -0800

    update

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  } else if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  }
-}
-assertEqual('Lighthouse Labs', 'Bootcamp')
-assertEqual(1, 1)
+const assertEqual = require('./assertEqual');
 
-function head (assertEqual) {
-  return assertEqual[0]
-}
-assertEqual(head([5, 6, 7]), 5)
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello')
+
+const head = function(assertEqual) {
+  return assertEqual[0];
+};
+
+
+module.exports = head;
