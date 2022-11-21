1ca06dc66764b97e6253f239e2b1a6a28cb0a9c0
Noor Chasib <noorchasib@gmail.com>
2022-11-21 14:33:20 -0800

    update

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
+// const assertArraysEqual = require('../assertArraysEqual');
+
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+// console.log(middle([1, 3, 9, 10, 15, 16, 18, 35, 16]));
+
+// assertArraysEqual((middle([3, 16, 18, 12, 14])), [16]);
+
+
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
-console.log(middle([1, 3, 9, 10, 15, 16, 18, 35, 16]));
-
-assertArraysEqual((middle([3, 16, 18, 12, 14])), [16]);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [6] for [5, 6, 7]", () => {
+    assert.deepEqual(middle([5, 6, 7]), [6]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+
+// const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+// tail(words);
+// assertEqual(words.length, 3);
+
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [6, 7, 8] for [5, 6, 7, 8]", () => {
+    assert.deepEqual(tail([5, 6, 7, 8]), [6, 7, 8]);
+  });
+});
